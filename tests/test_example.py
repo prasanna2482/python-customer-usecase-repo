4c12550ade74692b4e6a4813c502228f3348ceae
parinithaopsera456 <gparinitha@opsera.io>
2024-11-05 19:56:50 +0530

    Create test_example.py

---
+++ tests/test_example.py
@@ -0,0 +1,21 @@
+# tests/test_example.py
+
+def test_addition():
+    """Test addition of two numbers."""
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    """Test subtraction of two numbers."""
+    assert 2 - 1 == 1
+
+def test_multiplication():
+    """Test multiplication of two numbers."""
+    assert 2 * 3 == 6
+
+def test_division():
+    """Test division of two numbers."""
+    assert 6 / 2 == 3
+
+def test_string_concatenation():
+    """Test concatenation of two strings."""
+    assert "Hello" + " " + "World" == "Hello World"
